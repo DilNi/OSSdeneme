c0506983b617d4e47190873adc4183567d79485d
Yağmur Dilara Nişli <dil.nisli14@gmail.com>
2023-07-31 14:48:22 +0300

    eksikler düzeltildi.

--- public/js/script.js
+++ public/js/script.js
@@ -1,101 +1,3 @@
-const wrapper = document.querySelector('.wrapper');
-const wrapper1 = document.querySelector('.wrapper_1');
-const wrapper2 = document.querySelector('.wrapper');
-
-
-const btnPopup = document.querySelector('.btnLogin-popup');
-const btn1 = document.querySelector('.btn_1');
-const iconClose = document.querySelector('.icon-close');
-
-
-btnPopup.addEventListener('click', () => {
-  wrapper.classList.add('active-popup');
-  wrapper1.style.display = 'none';
-});
-
-iconClose.addEventListener('click', () => {
-  wrapper.classList.remove('active-popup');
-});
-
-btn1.addEventListener('click', () => {
-  window.location.href = "quiz.ejs";
-});
-
-
-// //matematik için script-------------------------------------------------
-// let currentIndex = 0; // Mevcut soru dizini
-// let mathQuestions = []; // Soruları tutacak dizi
-
-// const loadQuestionsBtn = document.getElementById('loadQuestionsBtn');
-// const difficultyContainer = document.getElementById('difficultyContainer');
-// const questionsContainer = document.getElementById('questionsContainer');
-// loadQuestionsBtn.addEventListener('click', async () => {
-//   const difficulty = document.getElementById('difficulty').value;
-//   try {
-//     const response = await fetch(`/getMathQuestions?difficulty=${difficulty}`);
-//     const data = await response.json();
-//     mathQuestions = data.mathQuestions;
-
-//     // Başlangıçta ilk soruyu göster
-//     currentIndex = 0;
-//     showQuestion(currentIndex);
-//     // Zorluk seçme kısmını gizle, soruların listelendiği bölümü göster
-//     difficultyContainer.style.display = 'none';
-//     questionsContainer.style.display = 'block';
-//   } catch (err) {
-//     console.error('Hata:', err);
-//   }
-// });
-// function showQuestion(index) {
-//   const questionsContainer = document.getElementById('questionsContainer');
-//   questionsContainer.innerHTML = ''; // Önceki soruyu temizle
-
-//   const question = mathQuestions[index];
-
-//   const questionTemplate = document.getElementById('questionTemplate').cloneNode(true);
-//   questionTemplate.style.display = 'block';
-
-//   const questionDescription = questionTemplate.querySelector('.questionDescription');
-//   questionDescription.textContent = question.description;
-
-//   const optionsContainer = questionTemplate.querySelector('.ml-md-3');
-//   optionsContainer.innerHTML = '';
-//   question.options.forEach((option, idx) => {
-//     const label = document.createElement('label');
-//     label.className = 'options';
-//     label.innerHTML = `
-// <span class="optionText">${option}</span>
-// <input type="radio" name="radio" value="${idx}">
-// <span class="checkmark"></span>
-// `;
-//     optionsContainer.appendChild(label);
-//   });
-
-//   const questionImage = questionTemplate.querySelector('#questionImage');
-//   // Soruya ait resim varsa, questionImage src'sine resim yolu ekleyin.
-//   if (question.resim_yol && question.resim_yol.data) {
-//     questionImage.src = `data:${question.resim_yol.contentType};base64,${question.resim_yol.data.toString('base64')}`;
-//   } else {
-//     // Soruya ait resim yoksa, resim alanını gizleyin.
-//     questionImage.style.display = 'none';
-//   }
-
-//   questionsContainer.appendChild(questionTemplate);
-// }
-
-
-
-// function navigateQuestion(direction) {
-//   currentIndex += direction;
-//   if (currentIndex < 0) {
-//     currentIndex = 0;
-//   } else if (currentIndex >= mathQuestions.length) {
-//     currentIndex = mathQuestions.length - 1;
-//   }
-//   showQuestion(currentIndex);
-// }
-
-  // script.js dosyası
 async function loadQuestionsForSubject(subject) {
   let currentIndex = 0; // Mevcut soru dizini
   let questions = []; // Soruları tutacak dizi
@@ -107,43 +9,9 @@ async function loadQuestionsForSubject(subject) {
   const questionDescription = questionTemplate.querySelector('.questionDescription');
   const optionsContainer = questionTemplate.querySelector('.ml-md-3');
   const questionImage = questionTemplate.querySelector('#questionImage');
-
-
- function showQuestion(index) {
-    questionsContainer.innerHTML = ''; // Önceki soruyu temizle
-
-    const question = questions[index];
-
-  
-    questionTemplate.style.display = 'block';
-
-    
-    questionDescription.textContent = question.description;
-
-   
-    optionsContainer.innerHTML = '';
-    question.options.forEach((option, idx) => {
-      const label = document.createElement('label');
-      label.className = 'options';
-      label.innerHTML = `
-  <span class="optionText">${option}</span>
-  <input type="radio" name="radio" value="${idx}">
-  <span class="checkmark"></span>
-  `;
-      optionsContainer.appendChild(label);
-    });
-//resim
-  
-    if (question.resim_yol && question.resim_yol.data) {
-      questionImage.src = `data:${question.resim_yol.contentType};base64,${question.resim_yol.data.toString('base64')}`;
-    } else {
-      questionImage.style.display = 'none';
-    }
-
-    questionsContainer.appendChild(questionTemplate);
-  }
-
-
+  const prevQuestionBtn = document.getElementById('prevQuestionBtn');
+  const nextQuestionBtn = document.getElementById('nextQuestionBtn');
+  const navigationButtons = document.querySelector('.navigation-buttons');
 
   loadQuestionsBtn.addEventListener('click', async () => {
     const difficulty = document.getElementById('difficulty').value;
@@ -155,47 +23,70 @@ async function loadQuestionsForSubject(subject) {
       // Başlangıçta ilk soruyu göster
       currentIndex = 0;
       showQuestion(currentIndex);
+
+
       // Zorluk seçme kısmını gizle, soruların listelendiği bölümü göster
       difficultyContainer.style.display = 'none';
       questionsContainer.style.display = 'block';
+      navigationButtons.style.display = 'flex';
+
+
     } catch (err) {
       console.error('Hata:', err);
     }
   });
+  // İleri ve geri butonları
+  nextQuestionBtn.addEventListener('click', () => {
+    if (currentIndex < questions.length - 1) {
+      currentIndex++;
+      showQuestion(currentIndex);
 
 
-  
-}
+    }
+  });
+  prevQuestionBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      showQuestion(currentIndex);
+
+    }
+  });
 
 
-function navigateQuestion(direction) {
-  let questions = [];
-  let currentIndex = 0;
-  currentIndex += direction;
-  if (currentIndex < 0) {
-    currentIndex = 0;
-  } else if (currentIndex >= questions.length) {
-    currentIndex = questions.length - 1;
+  if (currentIndex === questions.length - 1) {
+    nextQuestionBtn.disabled = true; // Son soruda
+  } else {
+    nextQuestionBtn.disabled = false;
   }
-  showQuestion(currentIndex);
+
+  function showQuestion(index) {
+    questionsContainer.innerHTML = ''; // Önceki soruyu temizle
+    const question = questions[index];
+    questionTemplate.style.display = 'block';
+    questionDescription.textContent = question.description;
+    optionsContainer.innerHTML = '';
+    question.options.forEach((option, idx) => {
+      const label = document.createElement('label');
+      label.className = 'options';
+      label.innerHTML = `
+  <span class="optionText">${option}</span>
+  <input type="radio" name="radio" value="${idx}">
+  <span class="checkmark"></span>
+  `;
+      optionsContainer.appendChild(label);
+    });
+    //resim
+
+    if (question.resim_yol && question.resim_yol.data) {
+      questionImage.src = `data:${question.resim_yol.contentType};base64,${question.resim_yol.data.toString('base64')}`;
+    } else {
+      questionImage.style.display = 'none';
+    }
+
+    questionsContainer.appendChild(questionTemplate);
+
+  }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-  
 
 
